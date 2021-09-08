ddefd6f8ff0775af4fd918a4a2dae4f69fb778d8
nidura <niduraprageeth@gmail.com>
2021-09-08 16:49:37 +0530

    init project

---
+++ s3-signed/src/main/java/com/aws/s3/S3SignedUrlApplication.java
@@ -0,0 +1,13 @@
+package com.aws.s3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S3SignedUrlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(S3SignedUrlApplication.class, args);
+    }
+
+}
---
+++ s3-signed/src/main/java/com/aws/s3/config/BucketAuthConfig.java
@@ -0,0 +1,8 @@
+package com.aws.s3.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BucketAuthConfig {
+    
+}
